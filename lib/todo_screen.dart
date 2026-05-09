75d934e10f95924ca30c8107afd3ed31a5441a56
sameer8287 <sameer.@sameers-MacBook-Air-2.local>
2026-05-09 11:44:59 +0530

    init

--- lib/todo_screen.dart
+++ lib/todo_screen.dart
@@ -34,30 +34,19 @@ class TodoScreen extends StatelessWidget {
                 trailing: Row(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-
                     /// update task
                     IconButton(
                       onPressed: () async {
-
                         final data = await Navigator.push(
                           context,
-                          MaterialPageRoute(
-                            builder: (_) => TodoForm(
-                              todoModel: value.task[index],
-                              index: index,
-                            ),
-                          ),
+                          MaterialPageRoute(builder: (_) => TodoForm(todoModel: value.task[index], index: index)),
                         );
 
                         if (data == null) return;
 
                         final todoModel = data as TodoModel;
 
-                        context.read<TodoProvider>().updateTask(
-                          index: index,
-                          title: todoModel.title,
-                          subtitle: todoModel.subtitle,
-                        );
+                        context.read<TodoProvider>().updateTask(index: index, title: todoModel.title, subtitle: todoModel.subtitle);
                       },
                       icon: Icon(Icons.edit, color: Colors.blue),
                     ),
